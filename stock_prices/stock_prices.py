fbe0cb1036aa2d91fd15f40ed4da9fbcbee09e46
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2020-02-19 17:20:29 -0500

    almost finished with stock_prices and recipe_batches; they both need tweaks

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,34 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  '''
+  Receives a list of stock prices as input
+  Return the maximum profit that can be made from a single buy and sell
+  Buy first before selling
+  '''
+
+  # TODO
+    # Find max number
+    # Find smallest number that occurs before the max number
+    # Subtract max number from smallest number 
+
+  largest = 0
+  smallest = 0
+  for i in range(len(prices)):
+    if prices[i] > prices[largest]:
+        largest = i
+        for j in range(largest):
+            if prices[j] < prices[smallest]:
+                smallest = j
+  print(prices[largest] - prices[smallest])
+
+
+# print(find_max_profit([1050, 270, 1540, 3800, 2])) # 3530
+find_max_profit([10, 7, 5, 8, 11, 9]) # 6
+find_max_profit([100, 90, 80, 50, 20, 10]) # -10
+find_max_profit([1050, 270, 1540, 3800, 2]) # 3530
+find_max_profit([100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79]) # 94
+
 
 
 if __name__ == '__main__':
