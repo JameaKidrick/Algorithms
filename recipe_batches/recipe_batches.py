2b3194a959b51558912da2926c5d32975f66454b
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2020-02-20 17:24:36 -0500

    finished recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,60 +2,36 @@
 
 import math
 
-# The keys will be the ingredient names, with their associated values being the amounts. In the case of the recipe dictionary, these amounts will represent the amount of each ingredient needed for the recipe, while in the case of the ingredients dictionary, the amounts represent the amounts available to you. 
+def recipe_batches(recipe, ingredients):
+        '''
+        Receives a recipe in the form of a dictionary
+        Receives ingredients you have available to you, also in the form of a dictionary
+        Return the maximum number of whole batches that can be made for the supplied recipe using the ingredients available to you
+        '''
 
-# Your function should output the maximum number of whole batches that can be made for the supplied recipe using the ingredients available to you, as indicated by the second dictionary. 
+        # base case: at least one of the ingredients will be negative
+        # get keys into list
+        # loop through list to get values
+        # divide values from each other
+        # add to array
+        # compare numbers to make sure there is enough ingredients
 
+        arr = []
+        smallest = 0
+        for key in recipe.keys():
+            if key not in ingredients.keys():
+                return 0
+        y = [key for key, value in recipe.items()]
+        for name in y:
+            arr.append(math.floor(ingredients[name]/recipe[name]))
+            for i in range(0, len(arr)):
+                if arr[i] < arr[smallest]:
+                    smallest = i
+        return arr[smallest]
 
-# ```python
-# # should return 0 since we don't have enough butter!
-# recipe_batches(
-#   { 'milk': 100, 'butter': 50, 'flour': 5 },
-#   { 'milk': 138, 'butter': 48, 'flour': 51 }
-# )
-# ```
-
-# ## Hints
-
-#  * What's the _minimum_ number of loops we need to perform in order to write a working implementation?
-
-def recipe_batches(recipe, ingredients, count=0):
-  '''
-  Receives a recipe in the form of a dictionary
-  Receives ingredients you have available to you, also in the form of a dictionary
-  Return the maximum number of whole batches that can be made for the supplied recipe using the ingredients available to you
-  '''
-  '''
-  base case: at least one of the ingredients will be negative
-  get keys into list
-  loop through list to get values
-  subtract used ingredients
-  add to counter
-  '''
-  
-  for key in recipe.keys():
-      if key not in ingredients.keys():
-          print(count)
-          return count
-      else:
-          ingredients[key] = ingredients[key] - recipe[key]
-          if ingredients[key] < 0:
-              print(count)
-              return count
-
-  count += 1
-
-  recipe_batches(recipe, ingredients, count)
-
-
-print(recipe_batches({ 'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5 }, { 'milk': 1288, 'flour': 9, 'sugar': 95 })) # 0
-print(recipe_batches({ 'milk': 100, 'butter': 50, 'cheese': 10 }, { 'milk': 198, 'butter': 52, 'cheese': 10 })) # 1
-print(recipe_batches({ 'milk': 2, 'sugar': 40, 'butter': 20 }, { 'milk': 5, 'sugar': 120, 'butter': 500 })) # 2
-print(recipe_batches({ 'milk': 2 }, { 'milk': 200})) # 100
-
-# if __name__ == '__main__':
-#   # Change the entries of these dictionaries to test 
-#   # your implementation with different inputs
-#   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-#   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-#   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+if __name__ == '__main__':
+    # Change the entries of these dictionaries to test 
+    # your implementation with different inputs
+    recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+    ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
